9241516e506144fad5a7b81f58d200b4ca88a594
Laney Smith <lsmith.unc@gmail.com>
2023-12-03 10:20:56 -0700

    test: add mode-toggle tests

---
+++ src/components/ui/mode-toggle.test.tsx
@@ -0,0 +1,38 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ModeToggle } from "components/ui/mode-toggle";
+import { ThemeProvider } from "./theme-provider";
+
+const wrapper = ({ children }: { children: React.ReactElement }) => (
+  <ThemeProvider>{children}</ThemeProvider>
+);
+
+describe("ModeToggle test", () => {
+  test("should toggle the theme class on the root element", async () => {
+    const user = userEvent.setup();
+    render(<ModeToggle />, { wrapper });
+
+    const themeButton = screen.getByRole("button", { name: "Toggle theme" });
+
+    expect(window.document.documentElement).toHaveClass("light");
+
+    await user.click(themeButton);
+
+    expect(window.document.documentElement).toHaveClass("dark");
+  });
+
+  test("should toggle the base font size class on the root element", async () => {
+    const user = userEvent.setup();
+    render(<ModeToggle />, { wrapper });
+
+    const baseFontSizeButton = screen.getByRole("button", {
+      name: "Toggle base font size",
+    });
+
+    expect(window.document.documentElement).toHaveClass("font-size-normal");
+
+    await user.click(baseFontSizeButton);
+
+    expect(window.document.documentElement).toHaveClass("font-size-big");
+  });
+});
