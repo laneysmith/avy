55904ced9bf826d0e5f9b100e1e9492ea7ebe601
Laney Smith <lsmith.unc@gmail.com>
2024-02-10 16:45:28 -0700

    feat: add forecast links

--- src/app/plan/02-anticipate-the-hazard.tsx
+++ src/app/plan/02-anticipate-the-hazard.tsx
@@ -1,5 +1,35 @@
+const listStyles = "mt-2 mb-4 ml-6 list-disc";
+const checklistHeadingStyles = "mt-4";
+
 const AnticipateTheHazard = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <h4 className={checklistHeadingStyles}>Colorado</h4>
+      <ul className={listStyles}>
+        <li>
+          <a
+            href="https://avalanche.state.co.us"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Colorado Avalanche Information Center
+          </a>
+        </li>
+      </ul>
+      <h4 className={checklistHeadingStyles}>Washington</h4>
+      <ul className={listStyles}>
+        <li>
+          <a
+            href="https://nwac.us/avalanche-forecast/#/all/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Northwest Avalanche Center
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default AnticipateTheHazard;
